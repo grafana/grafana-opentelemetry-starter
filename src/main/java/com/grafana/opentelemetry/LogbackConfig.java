f57ef2a3eac9732b66eb3fc3fd51f73621d01f95
gregor <gregor.zeitlinger@grafana.com>
2023-07-19 16:09:46 +0200

    spotless

--- src/main/java/com/grafana/opentelemetry/LogbackConfig.java
+++ src/main/java/com/grafana/opentelemetry/LogbackConfig.java
@@ -1,45 +1,47 @@
 package com.grafana.opentelemetry;
 
 import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 @ConditionalOnClass(name = "ch.qos.logback.classic.Logger")
 public class LogbackConfig implements LogAppenderConfigurer {
 
-    private static final Logger logger = LoggerFactory.getLogger(LogbackConfig.class);
+  private static final Logger logger = LoggerFactory.getLogger(LogbackConfig.class);
 
-    public void tryAddAppender() {
-        ch.qos.logback.classic.Logger logbackLogger = getLogger();
+  public void tryAddAppender() {
+    ch.qos.logback.classic.Logger logbackLogger = getLogger();
 
-        //check if appender has been added manually already
-        if (hasAppender(logbackLogger)) {
-            logger.info("logback OpenTelemetryAppender has already been added");
-            return;
-        }
-
-        logger.info("adding logback OpenTelemetryAppender");
-        OpenTelemetryAppender openTelemetryAppender = new OpenTelemetryAppender();
-        openTelemetryAppender.setCaptureExperimentalAttributes(true);
-        openTelemetryAppender.start();
-        logbackLogger.addAppender(openTelemetryAppender);
+    // check if appender has been added manually already
+    if (hasAppender(logbackLogger)) {
+      logger.info("logback OpenTelemetryAppender has already been added");
+      return;
     }
 
-    static ch.qos.logback.classic.Logger getLogger() {
-        return (ch.qos.logback.classic.Logger) LoggerFactory.getILoggerFactory().getLogger(Logger.ROOT_LOGGER_NAME);
-    }
+    logger.info("adding logback OpenTelemetryAppender");
+    OpenTelemetryAppender openTelemetryAppender = new OpenTelemetryAppender();
+    openTelemetryAppender.setCaptureExperimentalAttributes(true);
+    openTelemetryAppender.start();
+    logbackLogger.addAppender(openTelemetryAppender);
+  }
 
-    static boolean hasAppender(ch.qos.logback.classic.Logger logbackLogger) {
-        AtomicBoolean found = new AtomicBoolean(false);
-        logbackLogger.iteratorForAppenders().forEachRemaining(appender -> {
-            if (appender instanceof OpenTelemetryAppender) {
+  static ch.qos.logback.classic.Logger getLogger() {
+    return (ch.qos.logback.classic.Logger)
+        LoggerFactory.getILoggerFactory().getLogger(Logger.ROOT_LOGGER_NAME);
+  }
+
+  static boolean hasAppender(ch.qos.logback.classic.Logger logbackLogger) {
+    AtomicBoolean found = new AtomicBoolean(false);
+    logbackLogger
+        .iteratorForAppenders()
+        .forEachRemaining(
+            appender -> {
+              if (appender instanceof OpenTelemetryAppender) {
                 found.set(true);
-            }
-        });
-        return found.get();
-    }
-
+              }
+            });
+    return found.get();
+  }
 }
